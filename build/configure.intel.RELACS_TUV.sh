086fad3b4de621026e2056ff755a8d5358f0aeac
rpsouto <rpsouto@lncc.br>
2017-07-05 09:07:43 -0300

    Mudanças a serem submetidas: 	new file:   build/configure.intel.RELACS_TUV.sh 	new file:   src/jules/LIB/Makefile.comp.intel

---
+++ build/configure.intel.RELACS_TUV.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#!- configure for pgi 
+#./configure --program-prefix=BRAMS --enable-jules --with-fpcomp=/opt/hdf5-pgi12.6/bin/h5pfc --with-fcomp=/opt/pgi/osx86-64/12.6/bin/pgf90 \
+#--with-cpcomp=/opt/hdf5-pgi12.6/bin/h5pcc --with-ccomp=/opt/pgi/osx86-64/12.6/bin/pgcc --with-chem=RELACS_TUV --with-aer=SIMPLE
+#make
+
+DIR=$PWD
+
+#!- configure for intel
+./configure --prefix=${PWD} \
+ --program-prefix=BRAMS --enable-jules --with-fpcomp=h5pfc --with-fcomp=ifort \
+ --with-cpcomp=h5pcc --with-ccomp=icc --with-chem=RELACS_TUV --with-aer=SIMPLE
+
+
+
